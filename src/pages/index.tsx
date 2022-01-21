cbb7c55bb835b34b9e487556371b1d5bec972b46
Marco <maurelima@gmail.com>
2022-01-21 14:51:11 -0300

    Created home and Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,41 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        <Link href="/posts">
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.info}>
+              <img src="./calendar.png" alt="calendar" />
+              <time>15 Mar 2021</time>
+              <p>Joseph Oliveira</p>
+            </div>
+          </a>
+        </Link>
+        <Link href="/posts">
+          <a>
+            <strong>Criando um app CRA do zero</strong>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <div className={styles.info}>
+              <time>19 Abr 2021</time>
+              <p>Danilo Vieira</p>
+            </div>
+          </a>
+        </Link>
+      </div>
+      <div className={styles.keepRolling}>
+        <a>Carregar mais posts</a>
+      </div>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
